c3c31a0aaa7b706f7c8562774e52171704f3cdcd
lee-sihun <lee.sihun040223@gmail.com>
2025-05-27 16:42:54 +0900

    feat: createElement 함수 자동 임포트

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   esbuild: {
     jsx: "transform",
-    jsxFactory: "createElement",
+    jsxFactory: "createElement", // 커스텀 CreateElement 함수 지정
+    jsxInject: `import createElement from "@/vdom/createElements";`, // 자동 임포트
   },
 });
