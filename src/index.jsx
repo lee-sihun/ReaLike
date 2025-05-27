c00d18db25eeefacb9dab54aa8dcb38ea0378e8b
lee-sihun <lee.sihun040223@gmail.com>
2025-05-27 16:35:53 +0900

    feat: JSX 변환 테스트

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import createElement from "./vdom/createElements";
+import render from "./vdom/render";
+
+const vdom = (
+  <div id="app">
+    <h1>Hello, Virtual DOM with JSX!</h1>
+    <p>This is JSX syntax working with our custom virtual DOM.</p>
+  </div>
+);
+
+const container = document.getElementById("root");
+render(vdom, container);
